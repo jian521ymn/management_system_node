ccac58d779a7e93492083a0fdf4ebd4073f8808f
wb_jian <wb_jian@meituan.com>
2022-11-07 20:03:37 +0800

    333

--- routes/user/index.js
+++ routes/user/index.js
@@ -505,7 +505,7 @@ route.get('/InfoUploadList', (req, res) => {
         name:'NODE_DOWN',
         params:{isDelete:'0'},
     }
-    mysqlConnection({querySql:queryUserSql(params),res})
+    mysqlConnection({querySql:queryMyspl(params),res})
     .then(({result,total})=>{
         const list = result // 调用统一的用户信息map函数
         const listReduce = list?.reduce((prev,next)=>{
